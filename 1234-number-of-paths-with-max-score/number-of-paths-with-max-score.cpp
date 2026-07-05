e7d91d16cb42e6e24019dfa7d268a449ff29a27d
RiDiMa1 <ridimabudhlakoti488@gmail.com>
2026-07-05 23:56:44 +0530

    Time: 22 ms (31.71%) | Memory: 18.6 MB (25.09%) - LeetSync

---
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> pathsWithMaxScore(vector<string>& board) {
+        int n = board.size();
+        const int MOD = 1000000007;
+        vector<vector<int>> score(n, vector<int>(n, -1));
+        vector<vector<long long>> ways(n, vector<long long>(n, 0));
+        score[0][0] = 0;
+        ways[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+               if (board[i][j] == 'X')
+                    continue;
+                if (i == 0 && j == 0)
+                    continue;
+                int best = -1;
+                long long cnt = 0;
+                vector<pair<int,int>> parent = {
+                    {i-1,j},
+                    {i,j-1},
+                    {i-1,j-1}
+                };
+                for (auto p : parent) {
+                    int x = p.first;
+                    int y = p.second;
+                    if (x < 0 || y < 0)
+                        continue;
+                    if (score[x][y] == -1)
+                        continue;
+                    if (score[x][y] > best) {
+                       best = score[x][y];
+                        cnt = ways[x][y];
+                    }
+                    else if (score[x][y] == best) {
+                        cnt = (cnt + ways[x][y]) % MOD;
+                    }
+                }
+                if (best == -1)
+                    continue;
+                int value = 0;
+                if (board[i][j] >= '1' && board[i][j] <= '9')
+                    value = board[i][j] - '0';
+                score[i][j] = best + value;
+                ways[i][j] = cnt % MOD;
+            }
+        }
+        if (ways[n-1][n-1] == 0)
+            return {0,0};
+        return {score[n-1][n-1], (int)ways[n-1][n-1]};
+    }
+};
