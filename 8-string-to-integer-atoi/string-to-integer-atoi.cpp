98057c421a64800579df6a489e11e8f4103f5fb3
RiDiMa1 <ridimabudhlakoti488@gmail.com>
2026-07-10 00:28:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (55.90%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.size();
+
+        // Skip leading spaces
+        while (i < n && s[i] == ' ')
+            i++;
+
+        // Handle sign
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
+            i++;
+        }
+
+        long long result = 0;
+
+        // Convert digits
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            // Overflow check
+            if (result > (INT_MAX - digit) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return sign * result;
+    }
+};
